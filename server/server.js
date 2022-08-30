fcb5a8e06c068e4a01d9ee74de150963ce84f2a4
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-08-30 14:07:31 -0400

    server folders & install concurrently

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/htmlRoutes.js')(app);
+
+app.listen(PORT, function() {
+  console.log(`Now listening on port: ${PORT}`);
+});
